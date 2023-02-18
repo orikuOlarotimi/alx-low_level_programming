567bc28369eb86341df41a02c4cdf46fa732106c
orikuOlarotimi <rotimipraise07@gmail.com>
2023-02-18 03:35:14 -0800

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
- /**
+/**
  *main-entry point for the program 
  *Return: 0 if no error,return non zero if error
- */
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
