d0fe1c342d8dd3db01a624f948c0ff37c2ce69f1
orikuOlarotimi <rotimipraise07@gmail.com>
2023-02-18 03:57:38 -0800

    a good repo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n",
+printf("Size of a char: %ld byte(s)\n",
 sizeof(char));
-printf("Size of an int: %d byte(s)\n",
+printf("Size of an int: %ld byte(s)\n",
 sizeof(int));
-printf("Size of a long int: %d byte(s)\n",
+printf("Size of a long int: %ld byte(s)\n",
 sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n",
+printf("Size of a long long int: %ld byte(s)\n",
 sizeof(long long int));
-printf("Size of a float: %d byte(s)",
+printf("Size of a float: %ld byte(s)",
 sizeof(float));
 return (0);
 }
