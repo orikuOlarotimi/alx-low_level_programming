bf5fb68169a8e589ac1f8fa658149aa6973c663c
orikuOlarotimi <rotimipraise07@gmail.com>
2023-02-18 03:38:18 -0800

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
- *main-entry point for the program 
- *Return: 0 if no error,return non zero if error
+ *main-entry point for the program
+ *Return:0 if no error,return non zero if error
 */
 int main(void)
 {
