627673c90c8e0584b73e06677f174b890bdf92f1
orikuOlarotimi <rotimipraise07@gmail.com>
2023-02-19 07:11:49 -0800

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,24 +6,26 @@
  * main - A program that assign a random number to a variable
  * Return: 0 on sucess
  */
+
 int main(void)
 {
 	int n;
 	int l;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = (n % 10);
 	if ( l > 5)
 	{
-		printf("%d is greater than 5\n", l);
+		printf("Last digit of %d is greater than 5\n", n);
 	}
 	else if (l < 6 && l != 0)
 	{
-		printf("%d  is less than 6 and not 0", l);
+		printf(" Last digit of %d is less than 6 and not 0\n", n);
 	}
 	else
 	{
-		 printf("%d is 0", l);
+		 printf("Last digit of %d is 0\n", n);
 	}
 	return (0);
 }
