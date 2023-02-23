6951a6ec8149b8f4c8ff628743df9944617be1d2
orikuOlarotimi <rotimipraise07@gmail.com>
2023-02-22 21:45:31 -0800

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints put char
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ins[] = "_putchar";
+ 	int i = 0;
+
+	while (ins[i] != '\0')
+	{
+		_putchar(ins[i]);
+			i++;
+	}
+	_putchar('\n');
+	return (0);
+}
