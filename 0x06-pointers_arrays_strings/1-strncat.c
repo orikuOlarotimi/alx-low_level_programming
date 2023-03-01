4ae9f1815b86efe33498ecfbe1e2f8b871ea98d7
orikuOlarotimi <rotimipraise07@gmail.com>
2023-03-01 06:01:35 -0800

    a

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates string with n bytes from another string
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes to be copied
+ *
+ * Return: destination string concatenated
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
