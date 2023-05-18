725e2c00b52ded348a53fea5398df8ca6e57a1f7
orikuOlarotimi <rotimipraise07@gmail.com>
2023-05-18 13:29:35 -0700

    returns the number of elements lists in a doubly linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Counts the number of elements in a doubly-linked dlistint_t list.
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
  * @h: The head of the dlistint_t list.
  *
  * Return: The number of elements in the dlistint_t list.
@@ -18,4 +18,3 @@ size_t dlistint_len(const dlistint_t *h)
 
 	return (nodes);
 }
-
